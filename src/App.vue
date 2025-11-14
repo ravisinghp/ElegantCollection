391e5ae48fa6e7415a573e81fe110d2c1839c72c
rashmi1164 <rashmi.rana@planfirma.com>
2025-11-14 16:03:04 +0530

    Squashed 'RnD_FrontEnd/' content from commit a91f36c
    git-subtree-dir: RnD_FrontEnd
    git-subtree-split: a91f36c044e05651a66b0cc45d909dbfc6b8d355

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Outlook Email Viewer</h1>
+    <button @click="login">Login with Outlook</button>
+    <MailViewer />
+  </div>
+</template>
+
+<script>
+import MailViewer from './components/MailViewer.vue'
+import axios from 'axios'
+const restapi = import.meta.env.VITE_REST_API_ROOT;
+
+
+export default {
+  data() {
+    return {
+      restapiurl:restapi,
+    }
+  },
+  components: { MailViewer },
+  methods: {
+    async login() {
+      const res = await axios.get(this.restapiurl+"/auth/login")
+      window.location.href = res.data.auth_url
+    }
+  }
+}
+</script>
+
+
+
+
+<!-- <template>
+  <JobRecommendations />
+  <HelloWorld/>
+</template>
+
+<script setup>
+import JobRecommendations from './components/JobRecommendations.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+</script> -->
